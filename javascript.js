3d618f75e31a67d8913a0dd2620880f950c24960
=louisunny1328 <=louimon.sunny@nagarro.com>
2023-05-20 22:23:28 +0400

    DOM Manipulation

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
 const num = 6;
 const result = (num % 2 != 0) ? "odd" : "even";
 // display the result
-console.log(`Number is ${result}.`)
+console.log(`Number is ${result}.`);
+
+const div = document.createElement("div");
+div.innerText = "Hello Inner";
+div.textContent = "Hello Text Content"
+const body = document.body;
+body.append(div);
